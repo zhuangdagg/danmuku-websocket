71175ad9c6f67f64372f25bc4cb76c04cf584796
zhuang <1223445257@qq.com>
2024-03-26 23:29:08 +0800

    chore: rollup config;

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,22 @@
-import typescript from '@rollup/plugin-typescript'
-import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
 
 export default defineConfig({
-    input: 'index.ts',
-    output: [
-        {
-            file: './dist/bundle.js',
-            format: 'iife'
-        },
-        {
-            file: './dist/bundle.cjs',
-            format: 'cjs'
-        },
-        {
-            file: './dist/bundle.ejs',
-            format: 'es'
-        },
-    ],
-    plugins: [ typescript({ noEmit: true, noImplicitAny: true }) ]
-})
+  input: 'index.ts',
+  output: [
+    {
+      file: './dist/danmaku-websocket.global.js',
+      format: 'iife',
+    },
+    {
+      file: './dist/danmaku-websocket.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: './dist/danmaku-websocket.esm-bundle.js',
+      format: 'es',
+    },
+  ],
+
+  plugins: [typescript({ noEmit: true, noImplicitAny: true })],
+});
